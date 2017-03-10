4b00e6640f08ed0edb5f29b993b3f8a2921cc9c8
Oksanny <oksana.voitko7@gmail.com>
2017-03-10 13:32:45 +0200

    added button print

---
+++ Assets/Scripts/Print_Marker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Print_Marker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void OnClick()
+	{
+		Application.OpenURL("https://drive.google.com/file/d/0B4TdSrFJS3kcajljTEs5X1puSFE/view?usp=sharing");
+	}
+}
