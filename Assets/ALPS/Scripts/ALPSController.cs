4449a2f500668a27e9266feab1fff3066658859a
Oksanny <oksana.voitko7@gmail.com>
2017-03-20 17:56:36 +0200

    fixed normals

--- Assets/ALPS/Scripts/ALPSController.cs
+++ Assets/ALPS/Scripts/ALPSController.cs
@@ -145,8 +145,10 @@ public class ALPSController : MonoBehaviour {
 		cameraLeft.GetComponent<Camera>().depth = 0;
 		cameraRight.GetComponent<Camera>().depth = 1;
 
-	   // cameraLeft.GetComponent<Camera>().nearClipPlane = 0.01f;
-       // cameraRight.GetComponent<Camera>().nearClipPlane = 0.01f;
+	    cameraLeft.GetComponent<Camera>().nearClipPlane = 0.05f;
+        cameraRight.GetComponent<Camera>().nearClipPlane = 0.05f;
+		//cameraRight.GetComponent<Camera> ().farClipPlane = 7f;
+		//cameraLeft.GetComponent<Camera> ().farClipPlane = 7f;
 
 		GetComponent<Camera>().depth = Mathf.Max (cameraLeft.GetComponent<Camera>().depth, cameraRight.GetComponent<Camera>().depth) + 1;
 
